1d9cb21b49522c4cb658df63faf7f2cffeadc23a
Welinton Prediger <welintonpredigerdasilva@hotmail.com>
2021-11-13 16:38:24 -0300

    Start project.

---
+++ android/app/src/main/kotlin/com/example/project_personal_management/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.project_personal_management
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/views/home_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Column(
+          children: const [Text('Pre-Login Page')],
+        ),
+      ),
+    );
+  }
+}
